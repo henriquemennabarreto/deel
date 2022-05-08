4c51b9c76fef56e01c456fc5fae86c02fa96e844
Henrique Menna Barreto <hmennabarretoaraujo@gmail.com>
2022-05-08 01:43:00 -0300

    auth middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,13 @@
+
+const getProfile = async (req, res, next) => {
+    const { Profile } = req.app.get('models')
+    const profile = await Profile.findOne({ where: { id: req.get('profile_id') || 0 } })
+    if (!profile) return res.status(401).end()
+    req.profile = profile
+
+    const { userId } = req.params;
+    if (userId != undefined && userId != req.profile.id) return res.status(404).json({ message: "Invalid user" }).end();
+
+    next()
+}
+module.exports = { getProfile }
